f351bafe8c899fc313427cdd45779d4b2ccdafba
e11io11 <45672107+e11io11@users.noreply.github.com>
2019-06-13 16:24:32 +0200

    Delete jansitest.java

--- jansitest.java
+++
@@ -1,9 +0,0 @@
-import org.fusesource.jansi.*;
-
-public class jansitest{
-    public static void main(String[] args) {
-      AnsiConsole.out.println("\u001b[41;1m         \u001b[0m");
-      AnsiConsole.out.println("\u001b[41;1m    ♕    \u001b[0m");
-      AnsiConsole.out.println("\u001b[41;1m         \u001b[0m");
-    }
-}
